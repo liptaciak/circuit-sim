e51e778b87f984c1ed1015a9672b335884e45a1f
liptaciak <mateuszlipiec68@gmail.com>
2025-08-31 18:43:47 +0200

    circuit types definitions

--- include/circuit.h
+++ include/circuit.h
@@ -2,12 +2,40 @@
 #define __circuit__
 
 enum circuit_element_type_t {
-    Resistor
+    CONDUCTOR,
+    VSOURCE,
+    ISOURCE,
+    RESISTOR,
+    CAPACITOR,
+    INDUCTOR,
+    DIODE
 };
 
 struct circuit_element_t {
     enum circuit_element_type_t type;
+
+    // Used for rendering
+    float x;
+    float y;
+
+    union {
+        struct { }                     conductor;
+        struct { float resistance; }   resistor;
+        struct { float capacitance; }  capacitor;
+        struct { float inductance; }   inductor;
+        struct { float voltage_drop; } diode;
+        struct { float voltage; }      vsource;
+        struct { float current; }      isource;
+    };
+
+    void (*draw)(struct circuit_element_t* element);
     struct circuit_element_t** next;
+};
+
+struct circuit_t {
+    struct circuit_element_t* head;
+    struct circuit_element_t* elements;
+    size_t num_elements;
 };
 
 void process_circuit_element(struct circuit_element_t* element);
--- src/circuit.c
+++ src/circuit.c
@@ -4,34 +4,49 @@
 SDL_Window* window;
 SDL_Renderer* renderer;
 
+void process_circuit_element(struct circuit_element_t* element) {
+    // Process the circuit element (e.g., update its state, render it, etc.)
+}
+
 void main_loop(void* arg) {
-    SDL_Texture* tex = (SDL_Texture*)arg;
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    struct circuit_t state = (struct circuit_t*)arg;
 
-    SDL_Vertex verts[3] = {
-        {{400.0f, 225.0f}, {255, 0, 0, 254}, {0,0}},
-        {{300.0f, 400.0f}, {255, 0, 0, 254}, {0,0}},
-        {{500.0f, 400.0f}, {255, 0, 0, 254}, {0,0}}
-    };
-
-    int indices[3] = {0, 1, 2};
-    SDL_RenderGeometry(renderer, tex, verts, 3, indices, 3);
-    SDL_RenderPresent(renderer);
+    process_circuit_element(state.head);
 }
 
 int main() {
+    struct circuit_element_t source;
+    struct circuit_element_t resistor;
+
+    source.type = VSOURCE;
+    source.x = 100.0f;
+    source.y = 100.0f;
+    source.vsource.voltage = 5.0f;
+    source.draw = NULL;
+
+    resistor.type = RESISTOR;
+    resistor.x = 200.0f;
+    resistor.y = 200.0f;
+    resistor.resistor.resistance = 1000.0f;
+    resistor.draw = NULL;
+
+    struct circuit_element_t* source_next[] = { &resistor, NULL };
+    source.next = source_next;
+
+    struct circuit_element_t* resistor_next[] = { &source, NULL };
+    resistor.next = resistor_next;
+
     SDL_Init(SDL_INIT_VIDEO);
 
     window = SDL_CreateWindow("Circuit Simulator", 800, 600, 0);
     renderer = SDL_CreateRenderer(window, NULL);
 
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    struct circuit_t state;
+    
+    state.head = &source;
+    state.elements = &source;
+    state.num_elements = 2;
 
-    SDL_Texture* tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, 1, 1);
-    Uint32 white = 0xFFFFFFFF;
-    SDL_UpdateTexture(tex, NULL, &white, sizeof(Uint32));
-
-    emscripten_set_main_loop_arg(main_loop, tex, 0, 1);
+    emscripten_set_main_loop_arg(main_loop, &state, 0, 1);
     return 0;
 }
